df7259f57f56c19050775257c6a8ddb1755491c6
Arnoldmosha60 <arnoldmosha60@gmail.com>
2023-11-18 20:20:29 +0300

    Initial Commit

---
+++ React-Frontend/frontend/src/component/home.js
@@ -0,0 +1,26 @@
+// Import the react JS packages
+import {useEffect, useState} from "react";
+import axios from "axios";
+// Define the Login function.
+export const Home = () => {
+     const [message, setMessage] = useState('');
+     useEffect(() => {
+        if(localStorage.getItem('access_token') === null){                   
+            window.location.href = '/login'
+        }
+        else{
+         (async () => {
+           try {
+             const {data} = await axios.get(   
+                            'http://localhost:8000/home/', {
+                             headers: {
+                                'Content-Type': 'application/json'
+                             }}
+                           );
+             setMessage(data.message);
+          } catch (e) {
+            console.log('not auth')
+          }
+         })()};
+     }, []);
+}
