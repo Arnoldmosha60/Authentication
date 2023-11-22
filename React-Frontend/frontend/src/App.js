e2ba3ae467d78d9b0385aeae5479edc951253b47
Arnoldmosha60 <arnoldmosha60@gmail.com>
2023-11-22 12:45:48 +0300

    Second commit

--- React-Frontend/frontend/src/App.js
+++ React-Frontend/frontend/src/App.js
@@ -4,15 +4,17 @@ import {Login} from "./component/login";
 import {Home} from './component/home';
 import {Navigation} from './component/navigation';
 import {Logout} from './component/logout';
+import { Register } from './component/register';
 
 function App() {
     return (
       <BrowserRouter>
         <Navigation></Navigation>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/logout" element={<Logout/>}/>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/logout" element={<Logout />}/>
+          <Route path='/register' element={<Register />}/>
         </Routes>
       </BrowserRouter>
     )
--- React-Frontend/frontend/src/component/home.js
+++ React-Frontend/frontend/src/component/home.js
@@ -23,4 +23,8 @@ export const Home = () => {
           }
          })()};
      }, []);
+
+     return (
+      <h1>Welcome!! </h1>
+     )
 }
